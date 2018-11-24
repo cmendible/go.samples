88e8c73c29fd40f3971bc2a9b616a0e49d229187
cmendible <cmendible@gmail.com>
2018-11-24 21:58:53 +0100

    couhcbase-console sample

---
+++ couchbase-console/go.mod
@@ -0,0 +1,13 @@
+module couhcbase-console
+
+require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/google/uuid v1.1.0 // indirect
+	github.com/opentracing/opentracing-go v1.0.2 // indirect
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
+	golang.org/x/text v0.3.0 // indirect
+	gopkg.in/couchbase/gocb.v1 v1.5.1
+	gopkg.in/couchbase/gocbcore.v7 v7.1.9 // indirect
+	gopkg.in/couchbaselabs/gocbconnstr.v1 v1.0.2 // indirect
+	gopkg.in/couchbaselabs/jsonx.v1 v1.0.0 // indirect
+)
---
+++ couchbase-console/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/couchbase/gocb.v1"
+)
+
+type Beer struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Brewery string `json:"brewery_id"`
+}
+
+func main() {
+	cluster, _ := gocb.Connect("couchbase://localhost")
+	cluster.Authenticate(gocb.PasswordAuthenticator{
+		Username: "USERNAME",
+		Password: "PASSWORD",
+	})
+	bucket, _ := cluster.OpenBucket("beer-sample", "")
+
+	bucket.Manager("", "").CreatePrimaryIndex("", true, false)
+
+	// Create the beer document
+	beer := Beer{
+		ID:      "Polar Ice",
+		Name:    "Polar Ice",
+		Brewery: "Polar",
+	}
+
+	// Insert the beer document
+	bucket.Upsert("u:polarice", beer, 0)
+
+	fmt.Printf("Inserted document '%s'", beer.ID)
+
+	// Query the beer sample bucket and find the beer we just added.
+	query := gocb.NewN1qlQuery("SELECT name FROM beer-sample WHERE brewery_id =\"$1\"")
+	rows, _ := bucket.ExecuteN1qlQuery(query, []interface{}{"Polar"})
+	var row interface{}
+	for rows.Next(&row) {
+		fmt.Printf("Row: %v", row)
+	}
+}
