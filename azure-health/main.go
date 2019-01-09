e38e7fa91d3464b42f81eee1584a5b078ba7640d
cmendible <cmendible@gmail.com>
2019-01-09 19:23:00 +0100

    using category in the query

--- azure-health/main.go
+++ azure-health/main.go
@@ -16,47 +16,51 @@ func main() {
 	authorizer, err := auth.NewAuthorizerFromCLI()
 
 	// Create an ActivityLogs instance.
-	metricsClient := insights.NewActivityLogsClient("<subscriptionID>")
-	metricsClient.Authorizer = authorizer
+	logsClient := insights.NewActivityLogsClient("<subscriptionID>")
+	logsClient.Authorizer = authorizer
 
 	// Create the OData filter for a time interval and the Azure.Health Provider.
 	endTime := time.Now().UTC()
 	startTime := endTime.Add(time.Duration(-24) * time.Hour)
 	filter := fmt.Sprintf(
-		"eventTimestamp ge '%s' and eventTimestamp le '%s' and resourceProvider eq '%s'",
+		"eventTimestamp ge '%s' and eventTimestamp le '%s' and category eq '%s'",
 		startTime.Format(time.RFC3339),
 		endTime.Format(time.RFC3339),
-		"Azure.Health")
+		"ServiceHealth")
 
 	// Get the Events from Azure.
-	result, err := metricsClient.List(context.Background(), filter, "")
+	result, err := logsClient.List(context.Background(), filter, "")
 
-	if err == nil {
-		for result.NotDone() {
-			for _, eventData := range result.Values() {
-				if *eventData.Status.Value != "Resolved" && (eventData.Level == insights.Critical || eventData.Level == insights.Error) {
-					ct.Foreground(ct.Red, false)
-				} else if *eventData.Status.Value == "Resolved" {
-					ct.Foreground(ct.Green, false)
-				} else {
-					ct.Foreground(ct.White, false)
-				}
+	if err != nil {
+		panic(err)
+	}
 
-				fmt.Println(fmt.Sprintf(
-					"%s - %s - %s",
-					eventData.EventTimestamp.Local(),
-					*eventData.ResourceProviderName.Value,
-					*eventData.OperationName.Value))
-				fmt.Println(fmt.Sprintf("Status:\t %s", *eventData.Status.Value))
-				fmt.Println(fmt.Sprintf("Level:\t %s", eventData.Level))
-				fmt.Println(fmt.Sprintf("CorrelationId:\t %s", *eventData.CorrelationID))
-				fmt.Println(fmt.Sprintf("Resource Type:\t %s", *eventData.ResourceType.Value))
-				fmt.Println(fmt.Sprintf("Description:\t %s", *eventData.Description))
+	for result.NotDone() {
+		for _, eventData := range result.Values() {
+			if *eventData.Status.Value != "Resolved" && (eventData.Level == insights.Critical || eventData.Level == insights.Error) {
+				ct.Foreground(ct.Red, false)
+			} else if *eventData.Status.Value == "Resolved" {
+				ct.Foreground(ct.Green, false)
+			} else {
+				ct.Foreground(ct.White, false)
 			}
 
-			// Get more events if available.
-			result.NextWithContext(context.Background())
+			fmt.Println(fmt.Sprintf(
+				"%s - %s - %s",
+				eventData.EventTimestamp.Local(),
+				*eventData.ResourceProviderName.Value,
+				*eventData.OperationName.Value))
+			fmt.Println(fmt.Sprintf("Status:\t %s", *eventData.Status.Value))
+			fmt.Println(fmt.Sprintf("Level:\t %s", eventData.Level))
+			fmt.Println(fmt.Sprintf("CorrelationId:\t %s", *eventData.CorrelationID))
+			if eventData.ResourceType.Value != nil {
+				fmt.Println(fmt.Sprintf("Resource Type:\t %s", *eventData.ResourceType.Value))
+			}
+			fmt.Println(fmt.Sprintf("Description:\t %s", *eventData.Description))
 		}
+
+		// Get more events if available.
+		result.NextWithContext(context.Background())
 	}
 
 	ct.Foreground(ct.Green, false)
